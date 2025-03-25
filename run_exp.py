4495466d17b5429edb8486af6559315a3e476c98
Ricardo de Matos <ricardo.andre.de.matos@gmail.com>
2025-03-25 15:32:40 +0000

    list on exp search

--- run_exp.py
+++ run_exp.py
@@ -22,6 +22,8 @@ from models.NBeatsStackMoe import NBeatsStackMoe
 from neuralforecast.models import VanillaTransformer
 from neuralforecast.models import NBEATS
 
+from omegaconf import ListConfig
+
 def get_instance(name: str, best_params: dict[str, Any], horizon: int) -> BaseModel:
     if name.lower() == "nbeatsmoe":
         return NBeatsMoe(h=horizon, **best_params)
@@ -71,7 +73,7 @@ def save_results_summary(cfg, std_dev, median, results_file="results_summary.csv
     results_summary.to_csv(results_file, index=False)
 
 
-@hydra.main(config_path="conf", config_name="hyper.yaml")
+@hydra.main(config_path="conf", config_name="exp.yaml")
 def main(cfg: DictConfig):
     Y_ALL = load_dataset(cfg.dataset.name, cfg.dataset)
 
@@ -80,65 +82,73 @@ def main(cfg: DictConfig):
     dataset, *_ = TimeSeriesDataset.from_df(Y_train_df)
     test_dataset, *_ = TimeSeriesDataset.from_df(Y_test_df)
 
-    study_name = f"{cfg.model.name}_{cfg.dataset.name}_{cfg.dataset.group}_{cfg.horizon}"
-    
-    if cfg.model.name.lower() == "nbeatsstackmoe":
-        study_name = study_name.replace("stackmoe", "")
+    model_info = cfg.model
+    horizon = cfg.horizon
 
-    study = optuna.load_study(
-        study_name=study_name,
-        storage="sqlite:///c:/Users/ricar/mixture_of_experts_time_series/db/study.db",
-    )
+    models_names = model_info["name"] if isinstance(model_info["name"], ListConfig) else [model_info["name"]]
+    horizons = horizon if isinstance(horizon, ListConfig) else [horizon]
 
-    results = {"smape": [], "mae": [], "mse": []}
+    for model_name in models_names:
+        for horizon in horizons:
+            study_name = f"{model_name["name"]}_{cfg.dataset.name}_{cfg.dataset.group}_{horizon}"
+            
+            if model_name["name"].lower() == "nbeatsstackmoe":
+                study_name = study_name.replace("stackmoe", "")
 
-    list_random_seeds = random.sample(range(1, 1000), 20)
-    for i in range(20):
-        random_seed = list_random_seeds[i]
-        best_params = deepcopy(study.best_params)
-        best_params["random_seed"] = random_seed
+            study = optuna.load_study(
+                study_name=study_name,
+                storage="sqlite:///c:/Users/ricar/mixture_of_experts_time_series/db/study.db",
+            )
 
-        print(f"best_params: {best_params}")
+            results = {"smape": [], "mae": [], "mse": []}
 
-        model = get_instance(cfg.model.name, best_params, cfg.horizon)
+            list_random_seeds = random.sample(range(1, 1000), 20)
+            for i in range(20):
+                random_seed = list_random_seeds[i]
+                best_params = deepcopy(study.best_params)
+                best_params["random_seed"] = random_seed
 
-        fcst = NeuralForecast(models=[model], freq=cfg.dataset.freq)
-        fcst.fit(df=Y_train_df, static_df=None, val_size=cfg.horizon)
+                print(f"best_params: {best_params}")
 
-        # Evaluate on the test dataset
-        Y_pred_df = fcst.predict(futr_df=Y_test_df)
+                model = get_instance(model_name["name"], best_params, horizon)
 
-        y_true = Y_test_df['y'].values
-        y_hat = Y_pred_df[model.__class__.__name__].values
+                fcst = NeuralForecast(models=[model], freq=cfg.dataset.freq)
+                fcst.fit(df=Y_train_df, static_df=None, val_size=horizon)
 
-        n_series = Y_test_df['unique_id'].nunique()
-        y_true = y_true.reshape(n_series, -1)
-        y_hat = y_hat.reshape(n_series, -1)
+                # Evaluate on the test dataset
+                Y_pred_df = fcst.predict(futr_df=Y_test_df)
 
-        smape_e = smape(y_true, y_hat)
-        mae_e = mae(y_true, y_hat)
-        mse_e = mse(y_true, y_hat)
+                y_true = Y_test_df['y'].values
+                y_hat = Y_pred_df[model.__class__.__name__].values
 
-        results["smape"].append(smape_e)
-        results["mae"].append(mae_e)
-        results["mse"].append(mse_e)
+                n_series = Y_test_df['unique_id'].nunique()
+                y_true = y_true.reshape(n_series, -1)
+                y_hat = y_hat.reshape(n_series, -1)
 
-        print(f"results: {results}")
+                smape_e = smape(y_true, y_hat)
+                mae_e = mae(y_true, y_hat)
+                mse_e = mse(y_true, y_hat)
 
-    # Convert results to a DataFrame
-    results_df = pd.DataFrame(results)
+                results["smape"].append(smape_e)
+                results["mae"].append(mae_e)
+                results["mse"].append(mse_e)
 
-    # Calculate standard deviation and median for each metric
-    std_dev = results_df.std()
-    median = results_df.median()
+                print(f"results: {results}")
 
-    print("Standard Deviation:")
-    print(std_dev.round(4))
-    print("\nMedian:")   
-    print(median.round(4))
+            # Convert results to a DataFrame
+            results_df = pd.DataFrame(results)
 
-    # Save results to a CSV file with the specified columns
-    save_results_summary(cfg, std_dev, median, results_file="C:\\Users\\ricar\\mixture_of_experts_time_series\\results_summary.csv")
+            # Calculate standard deviation and median for each metric
+            std_dev = results_df.std()
+            median = results_df.median()
+
+            print("Standard Deviation:")
+            print(std_dev.round(4))
+            print("\nMedian:")   
+            print(median.round(4))
+
+            # Save results to a CSV file with the specified columns
+            save_results_summary(cfg, std_dev, median, results_file="C:\\Users\\ricar\\mixture_of_experts_time_series\\results_summary.csv")
 
 if __name__ == "__main__":
     main()
